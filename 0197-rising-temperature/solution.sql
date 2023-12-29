29fd8eba71e788c8525158a57a7ca76d7f16888b
Rispa Joseph <88330312+RispaJoseph@users.noreply.github.com>
2023-12-29 15:41:52 +0000

    Sync LeetCode submission - Rising Temperature - Runtime - 750 ms (6.67%), Memory - 0.0B (100.00%)

---
+++ 0197-rising-temperature/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT
+    a.Id
+FROM
+    Weather a,
+    Weather b
+WHERE
+    a.Temperature > b.Temperature
+    AND DATEDIFF(a.RecordDate, b.RecordDate) = 1;
