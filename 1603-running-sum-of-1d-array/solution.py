debd865fcd54c714d68511a3c511d8a611a53ac9
Rispa Joseph <88330312+RispaJoseph@users.noreply.github.com>
2024-01-03 05:02:56 +0000

    Sync LeetCode submission - Running Sum of 1d Array - Runtime - 85 ms (5.20%), Memory - 17.4 MB (87.99%)

---
+++ 1603-running-sum-of-1d-array/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        lst = []
+        a = 0 
+        for i in range(len(nums)):
+            a += nums[i]
+            lst.append(a)
+        return lst
