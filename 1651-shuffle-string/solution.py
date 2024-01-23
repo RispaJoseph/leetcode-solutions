d0ac5589be61e87ea709d410851eb8cd27713e3c
Rispa Joseph <88330312+RispaJoseph@users.noreply.github.com>
2024-01-23 05:09:28 +0000

    Sync LeetCode submission - Shuffle String - Runtime - 64 ms (6.92%), Memory - 16.5 MB (100.00%)

---
+++ 1651-shuffle-string/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        shuffle = ""
+        for i in range(len(indices)):
+            shuffle += (s[indices.index(i)])
+        return shuffle
