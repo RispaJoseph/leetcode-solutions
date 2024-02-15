18e4c55e9a5e2fbb6b2b4067b3a77dd44d5c6ece
Rispa Joseph <88330312+RispaJoseph@users.noreply.github.com>
2024-02-15 04:28:52 +0000

    Sync LeetCode submission - Third Maximum Number - Runtime - 50 ms (5.64%), Memory - 18.1 MB (93.25%)

---
+++ 0414-third-maximum-number/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = set(nums)
+        nums = sorted(nums,reverse = True)
+
+        if len(nums) >= 3:
+            return (nums[2])
+        else:
+            return (nums[0])
+
+
+      
