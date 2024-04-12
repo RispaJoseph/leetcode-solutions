0cc7a042a73ef47d3085bb23b21bad8df410c5d9
Rispa Joseph <88330312+RispaJoseph@users.noreply.github.com>
2024-04-12 12:14:12 +0000

    Sync LeetCode submission - First Unique Character in a String - Runtime - 67 ms (52.44%), Memory - 17.3 MB (100.00%)

---
+++ 0387-first-unique-character-in-a-string/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        count = Counter(s)
+        for i,j in count.items():
+            if j == 1:
+                return s.index(i)
+        return -1
+        
