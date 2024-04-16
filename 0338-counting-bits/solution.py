b0bbe36d140706822a9d55902e34d77658f24ec4
Rispa Joseph <88330312+RispaJoseph@users.noreply.github.com>
2024-04-16 04:16:52 +0000

    Sync LeetCode submission - Counting Bits - Runtime - 98 ms (5.06%), Memory - 23.2 MB (5.79%)

---
+++ 0338-counting-bits/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = []
+        for i in range(n+1):
+            a = bin(i)[2:]
+            count = 0
+            for digit in a:
+                if digit == '1':
+                    count += 1
+            ans.append(count)
+        return ans
+        
