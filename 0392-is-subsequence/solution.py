61c1296c0ccec5272bccd73a4df49d3c961caa45
Rispa Joseph <88330312+RispaJoseph@users.noreply.github.com>
2026-01-19 22:45:41 +0530

    Refactor isSubsequence method for clarity

--- 0392-is-subsequence/solution.py
+++ 0392-is-subsequence/solution.py
@@ -1,11 +1,10 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        len_of_str = len(s)
         i = 0
 
-        for j in range(len(t)):
-            if i < len_of_str and t[j] == s[i]:
+        for char in t:
+            if i < len(s) and s[i] == char:
                 i += 1
-
-        return i == len_of_str
+        return i == len(s)
+        
                 
